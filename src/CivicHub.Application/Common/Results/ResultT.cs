98b39ca3474fcc08d8fd472a1deb3cbe3d47e40d
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-05 03:41:41 +0400

    refactor: Update Result model to hold errors

--- src/CivicHub.Application/Common/Results/ResultT.cs
+++ src/CivicHub.Application/Common/Results/ResultT.cs
@@ -9,14 +9,21 @@ public class Result<TValue> : Result
         _value = value;
     }
 
-    protected internal Result(string errorMessage)
+    protected internal Result(List<Error> errorMessage)
         : base(errorMessage)
     {
+    }
 
+    protected internal Result(Error error)
+        : base(error)
+    {
     }
 
     public TValue Value => _value!;
 
     public static implicit operator Result<TValue>(TValue value) => new(value);
-
+    
+    public static implicit operator Result<TValue>(List<Error> errors) => new(errors);
+    
+    public static implicit operator Result<TValue>(Error error) => new(error);
 }
