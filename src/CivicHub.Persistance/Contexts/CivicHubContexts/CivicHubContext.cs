ec2c152a0775bcd4944f0f8b14cb37683e28708a
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-05 00:58:46 +0400

    feat: Add ability to apply all defined configurations to db context

--- src/CivicHub.Persistance/Contexts/CivicHubContexts/CivicHubContext.cs
+++ src/CivicHub.Persistance/Contexts/CivicHubContexts/CivicHubContext.cs
@@ -11,4 +11,11 @@ public class CivicHubContext(DbContextOptions<CivicHubContext> options) : DbCont
     public DbSet<Person> Persons { get; set; }
     public DbSet<PersonConnection> PersonConnections { get; set; }
     public DbSet<Location> Locations { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Apply all the configurations defined in Configurations folder
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CivicHubContext).Assembly);
+    }
 }
