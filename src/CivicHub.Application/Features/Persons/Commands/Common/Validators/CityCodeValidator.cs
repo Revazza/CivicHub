262a54195c84af934d7dc3e08c50d6f16c544258
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-05 23:55:48 +0400

    feat: Add city code validator to add person functionality

--- src/CivicHub.Application/Features/Persons/Commands/Common/Validators/CityCodeValidator.cs
+++ src/CivicHub.Application/Features/Persons/Commands/Common/Validators/CityCodeValidator.cs
@@ -1,15 +1,19 @@
 using CivicHub.Application.Features.Persons.Commands.Common.Extensions;
 using CivicHub.Domain.Cities;
+using CivicHub.Domain.Persons;
 using FluentValidation;
 
 namespace CivicHub.Application.Features.Persons.Commands.Common.Validators;
 
 public class CityCodeValidator : AbstractValidator<string>
 {
+    private const string PropertyName = nameof(Person.CityCode);
+    
     public CityCodeValidator()
     {
         RuleFor(cityCode => cityCode)
             .NotEmpty()
+            .WithName(PropertyName)
             .MinimumLength(CityConstraints.MinCityCodeLength)
             .MaximumLength(CityConstraints.MaxCityCodeLength)
             .MustBeEnglish();
