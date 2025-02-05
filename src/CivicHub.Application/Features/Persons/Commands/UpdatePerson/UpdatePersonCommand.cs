b0b1c3bc65a6de93929f6a5c14237a07da582c9e
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-06 01:48:25 +0400

    feat: Add UpdatePersonCommand.cs validator and move PhoneNumberDto.cs to common

--- src/CivicHub.Application/Features/Persons/Commands/UpdatePerson/UpdatePersonCommand.cs
+++ src/CivicHub.Application/Features/Persons/Commands/UpdatePerson/UpdatePersonCommand.cs
@@ -3,13 +3,13 @@ using CivicHub.Application.Features.Persons.Commands.Common.Dtos;
 using CivicHub.Domain.Persons.Enums;
 using MediatR;
 
-namespace CivicHub.Application.Features.Persons.Commands.AddPerson;
+namespace CivicHub.Application.Features.Persons.Commands.UpdatePerson;
 
-public record AddPersonCommand(
+public record UpdatePersonCommand(
     string FirstName,
     string LastName,
     string PersonalNumber,
     string CityCode,
     Gender Gender,
     DateTime BirthDate,
-    List<PhoneNumberDto> PhoneNumbers) : IRequest<Result<AddPersonResponse>>;
+    List<PhoneNumberDto> PhoneNumbers) : IRequest<Result<UpdatePersonResponse>>;
---
+++ src/CivicHub.Application/Features/Persons/Commands/UpdatePerson/UpdatePersonCommandValidator.cs
@@ -0,0 +1,35 @@
+using CivicHub.Application.Common.Extensions;
+using CivicHub.Application.Features.Persons.Commands.Common.Validators;
+using FluentValidation;
+
+namespace CivicHub.Application.Features.Persons.Commands.UpdatePerson;
+
+public class UpdatePersonCommandValidator : AbstractValidator<UpdatePersonCommand>
+{
+    public UpdatePersonCommandValidator()
+    {
+        RuleFor(person => person.FirstName)
+            .SetValidator(new FirstNameValidator())
+            .When(person => person.FirstName.IsNotNullOrEmpty());
+
+        RuleFor(person => person.LastName)
+            .SetValidator(new LastNameValidator())
+            .When(person => person.LastName.IsNotNullOrEmpty());
+
+        RuleFor(person => person.PersonalNumber)
+            .SetValidator(new PersonalNumberValidator())
+            .When(person => person.PersonalNumber.IsNotNullOrEmpty());
+
+        RuleFor(person => person.BirthDate)
+            .SetValidator(new AgeValidator())
+            .When(person => person.BirthDate.IsNotDefault());
+
+        RuleFor(person => person.CityCode)
+            .SetValidator(new CityCodeValidator())
+            .When(person => person.FirstName.IsNotNullOrEmpty());
+
+        RuleForEach(person => person.PhoneNumbers)
+            .SetValidator(new PhoneNumberDtoValidator())
+            .When(person => person.PhoneNumbers.IsNotEmpty());
+    }
+}
