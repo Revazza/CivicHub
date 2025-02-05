9199cde51fc82ffab997767979cc56e73c7314f2
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-05 18:56:41 +0400

    refactor: Increase max number length

--- src/CivicHub.Domain/Persons/ValueObjects/PhoneNumbers/PhoneNumberConstraints.cs
+++ src/CivicHub.Domain/Persons/ValueObjects/PhoneNumbers/PhoneNumberConstraints.cs
@@ -7,5 +7,5 @@ public static class PhoneNumberConstraints
     public const int MinAreaCodeLength = 1;
     public const int MaxAreaCodeLength = 5;
     public const int MinNumberLength = 1;
-    public const int MaxNumberLength = 5;
+    public const int MaxNumberLength = 10;
 }
