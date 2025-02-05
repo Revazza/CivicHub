0204fc78de4fd8c3c7f13cb8e9396ec490d93144
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-05 22:48:28 +0400

    feat: Add logging to ValidationPipelineBehaviour.cs and setup log levels

--- src/CivicHub.Application/Behaviours/ValidationPipelineBehaviour.cs
+++ src/CivicHub.Application/Behaviours/ValidationPipelineBehaviour.cs
@@ -1,6 +1,8 @@
 using CivicHub.Application.Common.Results;
 using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace CivicHub.Application.Behaviours;
 
@@ -8,10 +10,14 @@ public class ValidationPipelineBehaviour<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
+    private readonly ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> _logger;
     private readonly IValidator<TRequest>? _validator;
 
-    public ValidationPipelineBehaviour(IValidator<TRequest>? validator = null)
+    public ValidationPipelineBehaviour(
+        ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> logger,
+        IValidator<TRequest>? validator = null)
     {
+        _logger = logger;
         _validator = validator;
     }
 
@@ -23,10 +29,13 @@ public class ValidationPipelineBehaviour<TRequest, TResponse>
             return await next();
         }
 
+        _logger.LogInformation("Starting validating request: {RequestName}", typeof(TRequest).Name);
+
         var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
         if (validationResult.IsValid)
         {
+            _logger.LogInformation("Validation passed for request: {RequestName}", typeof(TRequest).Name);
             return await next();
         }
 
@@ -35,7 +44,18 @@ public class ValidationPipelineBehaviour<TRequest, TResponse>
             .SelectMany(group =>
                 group.Select(validationFailure => new Error(validationFailure.ErrorMessage, ErrorType.Validation)))
             .ToList();
+        
+        LogValidationFailures(errors);
 
         return (dynamic)errors;
+    }
+
+    private void LogValidationFailures(IEnumerable<Error> errors)
+    {
+        const string separator = " | ";
+        var errorMessages = string.Join(separator, errors.Select(x => x.Message));
+        
+        _logger.LogInformation("Validation failed for request: {RequestName}, Reason(s): {Reasons}",
+            typeof(TRequest).Name, errorMessages);
     }
 }
