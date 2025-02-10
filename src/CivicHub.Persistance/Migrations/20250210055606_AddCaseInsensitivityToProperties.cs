9c2c05bcbceb540d5c9b614573e0bc668bfe9b85
Sandro.Revazishvili <sandro.revazishviliii@gmail.com>
2025-02-10 10:36:19 +0400

    feat: Add migrations

---
+++ src/CivicHub.Persistance/Migrations/20250210055606_AddCaseInsensitivityToProperties.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CivicHub.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCaseInsensitivityToProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persons_Cities_CityCode",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "AreaCode",
+                table: "PhoneNumber");
+
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "PhoneNumber");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Persons",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Persons",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityCode",
+                table: "Persons",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "",
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConnectionType",
+                table: "PersonConnections",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Cities",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                collation: "SQL_Latin1_General_CP1_CI_AS",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persons_Cities_CityCode",
+                table: "Persons",
+                column: "CityCode",
+                principalTable: "Cities",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persons_Cities_CityCode",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AreaCode",
+                table: "PhoneNumber",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "PhoneNumber",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Persons",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Persons",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityCode",
+                table: "Persons",
+                type: "nvarchar(3)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConnectionType",
+                table: "PersonConnections",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Cities",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3,
+                oldCollation: "SQL_Latin1_General_CP1_CI_AS");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persons_Cities_CityCode",
+                table: "Persons",
+                column: "CityCode",
+                principalTable: "Cities",
+                principalColumn: "Code");
+        }
+    }
+}
